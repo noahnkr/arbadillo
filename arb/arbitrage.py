4483e770bdb2e1b4fbfd59858f2bd744e98b64c8
Noah Roberts <noahkrob97@gmail.com>
2024-07-25 00:00:11 -0500

    feat(arbitrage): Arbitrage Opportunity and ROI Calculation

--- arb/arbitrage.py
+++ arb/arbitrage.py
@@ -3,5 +3,28 @@ def american_to_decimal(american_odds):
     return american_odds / 100 + 1 if american_odds > 0 else \
            100 / american_odds + 1
 
-def decimal_to_ip(decimal_odds):
-    return (1 / decimal_odds) * 100
+def is_arbitrage_opportunity(odds1, odds2):
+    odds1 = american_to_decimal(odds1)
+    odds2 = american_to_decimal(odds2)
+    
+    # Calculate the implied probabilities.
+    ip1 = 1 / odds1
+    ip2 = 1 / odds2
+
+    return ip1 + ip2 < 1
+
+def calculate_arbitrage(odds1, odds2, investment):
+    odds1 = american_to_decimal(odds1)
+    odds2 = american_to_decimal(odds2)
+
+    ip1 = 1 / odds1
+    ip2 = 1 / odds2
+
+    stake1 = investment * ip1
+    stake2 = investment * ip2
+
+    profit = (stake1 * odds1) + (stake2 * odds2) - investment
+    roi = profit / investment * 100
+
+    return { "stake1": stake1, "stake2": stake2, "profit": profit, "roi": roi }
+ 
