5976c8451f129c362aa19de2206e4024dd176a9b
Noah Roberts <noahkrob97@gmail.com>
2024-08-20 00:55:34 -0500

    fix: removed duplicate execution  time

--- src/main.py
+++ src/main.py
@@ -12,14 +12,10 @@ django.setup()
 from api.models import Event, Sportsbook, Pick
 
 def main():
-    start_time = time.time()
     try:
         odds = scrape_odds(Config.LEAGUES, Config.BOOKS, Config.WEBDRIVER_THREADS)
-        logger.info(f'Successfully scraped odds for leagues: {Config.LEAGUES}')
     except Exception as e:
         logger.critical(f'Failed to scrape odds: {e}', exc_info=True)
-
-    logger.info(f'Execution Time: {time.time() - start_time}s')
 
     with open('data/export.json', 'w') as f:
         json.dump([o.to_dict() for o in odds], f, indent=4)
