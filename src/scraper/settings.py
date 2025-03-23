1e85edc0192964333ad2d22c8d4cd3340348f4e5
Noah Roberts <noahnkr@iastate.edu>
2025-03-23 01:57:53 -0500

    configured scrapy module structure with Docker

--- src/scraper/settings.py
+++ src/scraper/settings.py
@@ -1,19 +1,9 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 BOT_NAME = "scraper"
+
 SPIDER_MODULES = ["scraper.spiders"]
 NEWSPIDER_MODULE = "scraper.spiders"
 
-# Redis
-REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
-REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+ROBOTSTXT_OBEY = True
 
-# PostgreSQL for persistent storage
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
-POSTGRES_PORT = int(os.getenv("POSTGRES_PORT", 5432))
-POSTGRES_DB = os.getenv("POSTGRES_DB", "sportsbook")
-POSTGRES_USER = os.getenv("POSTGRES_USER", "user")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "password")
+TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+FEED_EXPORT_ENCODING = "utf-8"
