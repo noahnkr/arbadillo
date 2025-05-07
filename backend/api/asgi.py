7773ac46acaaa0cb7cf6afafdb556e8068a99a29
Noah Roberts <noahkrob97@gmail.com>
2025-05-07 13:34:53 -0500

    restructured Django directory

---
+++ backend/api/asgi.py
@@ -0,0 +1,7 @@
+import os
+
+env = os.getenv('DJANGO_ENV', 'dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'api.settings.{env}')
+
+from django.core.asgi import get_asgi_application
+application = get_asgi_application()
