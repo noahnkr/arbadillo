28a25d39238a47d4a5b6ba29dc16be03c33ff614
Noah Roberts <noahkrob97@gmail.com>
2025-05-07 14:36:13 -0500

    moved scrapy project to backend folder

--- scraper/common/urls.py
+++
@@ -1,9 +0,0 @@
-SCHEDULE_URLS = {
-	"nfl": "https://www.espn.com/nfl/schedule",
-	"nba": "https://www.espn.com/nba/schedule",
-	"mlb": "https://www.espn.com/mlb/schedule",
-	"nhl": "https://www.espn.com/nhl/schedule",
-    "ncaam": "https://www.espn.com/mens-college-basketball/schedule",
-    "ncaaw": "https://www.espn.com/womens-college-basketball/schedule",
-	"ncaaf": "https://www.espn.com/college-football/schedule",
-}
