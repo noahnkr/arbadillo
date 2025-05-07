b0fa0a5d8775518928aefb7c190393ac3934c82b
Noah Roberts <noahnkr@iastate.edu>
2025-05-07 14:36:13 -0500

    moved scrapy project to backend folder

---
+++ backend/scraper/settings.py
@@ -0,0 +1,9 @@
+BOT_NAME = 'scraper'
+
+SPIDER_MODULES = ['spiders']
+NEWSPIDER_MODULE = 'spiders'
+
+ROBOTSTXT_OBEY = False
+
+TWISTED_REACTOR = 'twisted.internet.asyncioreactor.AsyncioSelectorReactor'
+FEED_EXPORT_ENCODING = 'utf-8'
