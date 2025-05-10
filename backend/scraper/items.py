5b8f9c0fd1326fff842375a7ee0670c06b979401
Noah Roberts <noahnkr@iastate.edu>
2025-05-09 19:19:48 -0500

    scrapy items

---
+++ backend/scraper/items.py
@@ -0,0 +1,21 @@
+import scrapy
+
+class EventItem(scrapy.Item):
+    event_key = scrapy.Field()
+    league = scrapy.Field()
+    start_time = scrapy.Field()
+    away_team = scrapy.Field()
+    home_team = scrapy.Field()
+    status = scrapy.Field()
+    collected_at =  scrapy.Field()
+
+class OddsItem(scrapy.Item):
+    event_key = scrapy.Field()
+    sportsbook = scrapy.Field()
+    market = scrapy.Field()
+    outcome = scrapy.Field()
+    value = scrapy.Field()
+    line = scrapy.Field()
+    player = scrapy.Field()
+    prop = scrapy.Field()
+    collected_at = scrapy.Field()
