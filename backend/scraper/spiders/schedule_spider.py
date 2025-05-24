22b5387e473d06f058a2d10c4a1bd84694774c6f
Noah Roberts <noahkrob97@gmail.com>
2025-05-24 12:58:40 -0500

    refactored redis keys, decoded responses, extra comments

--- backend/scraper/spiders/schedule_spider.py
+++ backend/scraper/spiders/schedule_spider.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from redis import Redis
 from settings import REDIS_HOST, REDIS_PORT
 from common.constants import SCHEDULE_URLS
-from common.utils import create_event_key, normalize_team_name, current_timestamp
+from common.utils import create_event_key, normalize_team_name, current_timestamp, generate_events_hash
 from items import EventItem
 
 class ScheduleSpider(scrapy.Spider):
@@ -12,7 +12,7 @@ class ScheduleSpider(scrapy.Spider):
 	
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
-		self.redis = Redis(host=REDIS_HOST, port=REDIS_PORT)
+		self.redis = Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
 
 	def start_requests(self):
@@ -34,18 +34,22 @@ class ScheduleSpider(scrapy.Spider):
 			except ValueError:
 				continue # Skip unrecognized date formats
 
-			rows = schedule.css('tbody.Table__TBODY tr')
-			for row in rows:
+			for row in schedule.css('tbody.Table__TBODY tr'):
 				event = self._parse_row(row, league, event_date.strftime('%Y-%m-%d'))
 				if event:
-					# Cache event in redis
+					redis_key = 'schedule:events'
 					event_key = event['event_key']
-					redis_key = f'events:{event_key}'
-					self.redis.set(redis_key, json.dumps(dict(event)), ex=60 * 60 * 24)
+					prev_event = self.redis.hget(redis_key, event_key)
 
-					# Trigger async insert/update
-					# update_event_in_db.delay(event)
-					print(event)
+					if prev_event is None:
+						# Event hasn't been cached yet, insert row into DB
+						pass
+					elif generate_events_hash(dict(event)) != generate_events_hash(json.loads(prev_event)): 
+						# Event info has changed, update row in DB
+						pass
+					
+					# Update event cache
+					self.redis.hset(redis_key, event_key, json.dumps(dict(event)))
 					yield event
 
 
@@ -56,16 +60,22 @@ class ScheduleSpider(scrapy.Spider):
 				return None
 
 			# Format team names
-			away = normalize_team_name(teams[0].attrib['href'].split('/')[6], league)
-			home = normalize_team_name(teams[1].attrib['href'].split('/')[6], league)
-		except Exception as e:
-			print('Error getting team names:', e)
+			away = normalize_team_name(teams[0].attrib.get('href', '').split('/')[6], league)
+			home = normalize_team_name(teams[1].attrib.get('href', '').split('/')[6], league)
+		except Exception:
 			return None
+
 		event_key = create_event_key(league, date, away, home)
 
 		# Determine start time and status
 		raw_time = row.css('td.date__col a::text').get()
 		start_time, status = self._parse_start_time(date, raw_time, event_key)
+
+		# Update cache of upcoming/active events
+		if status in ['upcoming', 'active']:
+			self.redis.sadd('schedule:events:active', event_key)
+		else:
+			self.redis.srem('schedule:events:active', event_key)
 
 		return EventItem(
             event_key=event_key,
@@ -79,33 +89,33 @@ class ScheduleSpider(scrapy.Spider):
 	
 
 	def _parse_start_time(self, date: datetime, time_str: str, event_key: str) -> tuple[str, str]:
-		redis_key = f'start_time:{event_key}'
+		redis_key = f'schedule:events:{event_key}:start_time'
 
 		if time_str:
 			if time_str.strip().upper() == 'LIVE':
-				cached_time = self.redis.get(redis_key)
-				start_time = cached_time.decode('utf-8') if cached_time else None
-				self.redis.sadd('events:active', event_key)
+				# Start time is not shown on page, we must look it up in cache
+				# (note: on initial run, there will be no cached time and thus start_time will be None)
+				start_time = self.redis.get(redis_key)
 				status = 'active'
 			else:
+				# Start time is shown on page
 				try:
 					parsed_time = datetime.strptime(time_str.strip(), '%I:%M %p').time()
 					combined = datetime.combine(date, parsed_time)
 					start_time = combined.strftime('%Y-%m-%dT%H:%M')
 
-					# Check if stored time is different and update if needed
 					prev_time = self.redis.get(redis_key)
-					if not prev_time or prev_time.decode('utf-8') != start_time:
+					if not prev_time or prev_time != start_time:
+						# Cached start time either doesn't exist or has changed 
 						self.redis.set(redis_key, start_time, ex=60 * 60 * 24)
 
-					self.redis.sadd('events:active', event_key)
 					status = 'upcoming'
 				except ValueError:
+					# Exception occured while parsing start time
 					return None, None
 		else:
-			cached_time = self.redis.get(redis_key)
-			start_time = cached_time.decode('utf-8') if cached_time else None
-			self.redis.srem('events:active', event_key)
+			# Time element doesn't exist on page, thus the event must be completed
+			start_time = self.redis.get(redis_key)
 			status = 'completed'
 
 		return start_time, status
