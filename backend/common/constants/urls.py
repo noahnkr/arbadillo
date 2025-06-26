ace1dff29f68beeb40e4e8cd7d405951aaa49df6
Noah Roberts <noahkrob97@gmail.com>
2025-06-25 19:14:22 -0500

    removed unused urls

--- backend/common/constants/urls.py
+++ backend/common/constants/urls.py
@@ -1,59 +1,29 @@
 ESPN_URLS = {
-    'nfl': '',
     'nba': 'https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard',
     'mlb': 'https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard',
-    'mls': '',
-    'nhl': '',
-    'ncaaf': '',
-    'ncaab': '',
-    'ncaaw': '',
 }
 
 ESPNBET_URLS = {
     'base': 'https://sportsbook-tsb.ca-default.thescore.bet/graphql/persisted_queries/4e63acef22373225db328b8fc6534a998cd73a90745758017559fa4b43e1fe66',
-    'nfl': '',
     'nba': '/sport/basketball/organization/united-states/competition/nba',
     'mlb': '/sport/baseball/organization/united-states/competition/mlb',
-    'mls': '',
-    'nhl': '',
-    'ncaaf': '',
-    'ncaab': '',
-    'ncaaw': '',
 }
 ESPNBET_AUTH_TOKEN = 'Bearer eyJhbGciOiJSU0EtT0FFUCIsImVuYyI6IkExMjhDQkMtSFMyNTYifQ.I90O69ULGH1ehEsPEpXv88G-0YYSnvlTKb2NL-38EvZU66NSOWsxWZXkOg4QpbAuyooucKAhMYmSwQmIJ2iEJ0U-NZP7upAyI1-riFZM26h5i5i58cXGDFqTYqU3sg6imTgsh0CFo_LsSwMAzcUAubpeCXH_TaPtHneme2jjPoYvo-fwt_OanVcMVqQnVwbd7rQktGDM-NBYQO2DQdegCA_n9lyQKeJHgoYgXnN426od2-MCVpc--E7fwz1-0fQo4eeaI0BEi3Oxaykxc3aPD4dtJA4CpGL9VKxe-DCa_A-e3TYGrzKRbtUgjlyHNAXtjP8XF6PBR3Dk2FG_VKyBZuZwQdSwtNT5cbi6OjSa-n32ArCXueqMygz_51Fc-kP34sU2mxC_XoN9bvSOXIu3iyLXZfVdGZOpRNwAMxH0yhmRx0KB89Vr9nSwbTAyqX693bnkIeNoaASK_iuptNXcVsg6HyE93xceTrT7ALmQrZW5Z0V2brTWNnhdgypRYy9fMxYb6Y0T3nbeuNvMSHtQUj5H9ZERJuhDk4oe7Eu6s-U2bfjSO9R2yxUs6i-VS58cqUPnK0HxSzSFiwVUYRu7x6ZLEW7ZXL5vgYe4A13uTc-CTyoJiIHi_xuHcPfUX2pHfXsVa-kcXBls_Bljr4NBNdhEzC3OS28K2H7sD4gv3T8.DAZz4Z6i1ZeSoknKSeNayA.3dlCcWPnwYjTpjirq3ml7b_3Ry_9Y9lev7sDh6yS-Ty_clXkn9ULinDnxVUQQYzIW69HFf7CKO6UmHO1tGgVPNjhCN9_nQbbRrv4GbcnhfHQ7-r6VdsscX7ikwKyGTfYPzHDWzUg6z8uZBd_llb747myH55kt_fpzUNEwd3y530-6NZ1MhZHymIG8htWzf-JvbcmU_ff7V15p6iMHByeSR11aJQ0nAhURlw0fTuvg9N4SSmMWh3I-Wg5YvvrmFq0kmsO71vxuSJABKnp7yG7FQ.LAZPf8A0B73LnIZIn40Fmw'
 
 BETMGM_URLS = {
-    'nfl': '',
     'nba': 'https://sports.il.betmgm.com/en/sports/basketball-7/betting/usa-9/nba-6004',
     'mlb': 'https://sports.il.betmgm.com/en/sports/baseball-23/betting/usa-9/mlb-75',
-    'mls': '',
-    'nhl': '',
-    'ncaaf': '',
-    'ncaab': '',
-    'ncaaw': '',
 }
 
 DRAFTKINGS_URLS = {
-    'nfl': '',
     'nba': 'https://sportsbook-nash.draftkings.com/api/sportscontent/dkusil/v1/leagues/42648',
     'mlb': 'https://sportsbook-nash.draftkings.com/api/sportscontent/dkusil/v1/leagues/84240',
-    'mls': '',
-    'nhl': '',
-    'ncaaf': '',
-    'ncaab': '',
-    'ncaaw': '',
     'event': 'https://sportsbook-nash.draftkings.com/api/sportscontent/dkusil/v1/events',
 }
 
 FANDUEL_URLS = {
-    'nfl': '',
     'nba': 'https://sbapi.il.sportsbook.fanduel.com/api/content-managed-page?page=CUSTOM&customPageId=nba',
     'mlb': 'https://sbapi.il.sportsbook.fanduel.com/api/content-managed-page?page=CUSTOM&customPageId=mlb',
-    'mls': '',
-    'nhl': '',
-    'ncaaf': '',
-    'ncaab': '',
-    'ncaaw': '',
     'event': 'https://sbapi.il.sportsbook.fanduel.com/api/event-page',
 }
 FANDUEL_AUTH_TOKEN = 'FhMFpcPWXMeyZxOx'
