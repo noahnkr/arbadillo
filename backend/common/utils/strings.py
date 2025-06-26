593e25853e61907ba32194c82face8ebeb558deb
Noah Roberts <noahkrob97@gmail.com>
2025-06-25 19:15:34 -0500

    added None check to string utiltiy functions

--- backend/common/utils/strings.py
+++ backend/common/utils/strings.py
@@ -2,18 +2,23 @@ import re
 
 def extract_float(raw: str) -> float | None:
     """Extracts the first numeric value from a string and returns it as a float."""
-    match = re.search(r'\d+(?:\.\d+)?', raw)
+    if raw is None:
+        return None
+    match = re.search(r'(?:\-|\+)?\d+(?:\.\d+)?', raw)
     return float(match.group()) if match else None
 
 
 def extract_text(raw: str) -> str | None:
     """Extracts the first alpha values from a string."""
+    if raw is None:
+        return None
     match = re.search(r'[a-z\s]*', raw)
-    return float(match.group().strip()) if match else None
+    return match.group().strip() if match else None
 
 
 def clean_str(raw: str) -> str:
-    """Trim and normalize input string extracted from web and JSON"""
-    raw = re.sub(r"[-_e/\\]", " ", raw)
-    raw = re.sub(r"\s+", " ", raw)
+    """Trims white space from input string extracted from web and JSON"""
+    if raw is None:
+        return None
+    raw = re.sub(r'\s+', ' ', raw)
     return raw.strip().lower()
