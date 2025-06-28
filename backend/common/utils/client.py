a419a0c314430f3f8cc65abd687ff18013bf9717
Noah Roberts <noahkrob97@gmail.com>
2025-06-28 15:24:16 -0500

    created singleton instance of Playwright, removed PlaywrightSessionManager

--- backend/common/utils/client.py
+++ backend/common/utils/client.py
@@ -4,6 +4,17 @@ from playwright.sync_api import sync_playwright
 
 from common.constants.sportsbook import CLIENT_MAP
 
+_browser = None
+_playwright = None
+
+def get_browser():
+    global _browser, _playwright
+    if _browser is None:
+        _playwright = sync_playwright().start()
+        _browser = _playwright.chromium.launch(headless=True)
+    return _browser
+
+
 def get_class_from_path(path: str):
     module_path, class_name = path.rsplit('.', 1)
     module = importlib.import_module(module_path)
@@ -16,49 +27,3 @@ def get_client(sportsbook: str, sport: str, league: str):
         raise ValueError(f'No client found for sportsbook `{sportsbook}`')
     ClientClass = get_class_from_path(class_path)
     return ClientClass(sport, league)
-
-
-class PlaywrightSessionManager:
-    _instance = None
-
-    def __init__(self):
-        self.p = sync_playwright().start()
-        self.browser = self.p.chromium.launch(headless=True)
-        self.context = self.browser.new_context()
-        self.page = self.context.new_page()
-
-
-    @classmethod
-    def get_instance(cls, force_new=False):
-        if cls._instance is None or force_new:
-            if cls._instance:
-                cls.shutdown()
-            cls._instance = PlaywrightSessionManager()
-        return cls._instance
-
-
-    @classmethod
-    def new_page(cls, force_context=False):
-        if force_context:
-            cls.get_instance(force_new=True)
-        elif cls._instance is None:
-            cls.get_instance()
-        return cls._instance.context.new_page()
-
-
-    @classmethod
-    def get_cookies(cls, force_new=False):
-        if cls._instance is None or force_new:
-            cls.get_instance(force_new=True)
-            cls._instance.page.wait_for_timeout(3000)
-
-        cookies = cls._instance.context.cookies()
-        return '; '.join(f"{c['name']}={c['value']}" for c in cookies)
-
-
-    @classmethod
-    def shutdown(cls):
-        if cls._instance:
-            cls._instance.browser.close()
-            cls._instance.p.stop()
-            cls._instance = None
