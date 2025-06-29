46e3d4fb163b7e67aac7c2bbdd0cd15dd4510d5b
Noah Roberts <noahkrob97@gmail.com>
2025-06-29 03:42:24 -0500

    removed string representation class functions

--- backend/sports/models.py
+++ backend/sports/models.py
@@ -6,9 +6,6 @@ class Team(models.Model):
     league = models.CharField(max_length=20)
     name = models.CharField(max_length=100)
 
-    def __str__(self):
-        return f'{self.league.upper()} - {self.name}'
-
 
 class Player(models.Model):
     espn_id = models.CharField(max_length=20, unique=True)
@@ -17,10 +14,6 @@ class Player(models.Model):
     name = models.CharField(max_length=100)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     position = models.CharField(max_length=100, null=True, blank=True)
-    collected_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
 
 
 class Event(models.Model):
@@ -34,11 +27,6 @@ class Event(models.Model):
     league = models.CharField(max_length=20)
     away_team = models.ForeignKey(Team, related_name='away_events', on_delete=models.CASCADE)
     home_team = models.ForeignKey(Team, related_name='home_events', on_delete=models.CASCADE)
-
     start_time = models.DateTimeField()
     status = models.CharField(max_length=10, choices=Status.choices, default=Status.UPCOMING)
-
     collected_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f'{self.league.upper()} - {self.away_team} @ {self.home_team} ({self.start_time})'
