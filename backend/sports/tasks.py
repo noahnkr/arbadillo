918db57c1812b2990608782034d7d6dc01ea2680
Noah Roberts <noahkrob97@gmail.com>
2025-06-29 14:25:58 -0500

    fix: update sync functions to handle upserted teams, events, and players correctly

--- backend/sports/tasks.py
+++ backend/sports/tasks.py
@@ -12,10 +12,10 @@ logger = logging.getLogger(__name__)
 def sync_teams(sport: str, league: str):
 	logger.info('Syncing teams...')
 	client = ESPNClient(sport, league)
-	teams = client.get_teams()
+	teams, upsert_teams = client.get_teams()
 
-	logger.info(f'Upserting {len(teams)} team(s)')
-	for team in teams:
+	logger.info(f'Upserting {len(upsert_teams)} team(s)')
+	for team in upsert_teams:
 		Team.objects.update_or_create(
 			team_key=team.team_key,
 			defaults={
@@ -24,17 +24,17 @@ def sync_teams(sport: str, league: str):
 				'name': team.name,
 			}
 		)
-	return [team.to_dict() for team in teams]
+	return [t.to_dict() for t in teams]
 
 
 @shared_task(queue='scraping')
 def sync_schedule(sport: str, league: str):
 	logger.info('Syncing ESPN schedule...')
 	client = ESPNClient(sport, league)
-	events = client.get_schedule()
+	_, upsert_events = client.get_schedule()
 
-	logger.info(f'Upserting {len(events)} event(s)')
-	for event in events:
+	logger.info(f'Upserting {len(upsert_events)} event(s)')
+	for event in upsert_events:
 		try:
 			away_team = Team.objects.get(team_key=event.away_team_key)
 			home_team = Team.objects.get(team_key=event.home_team_key)
@@ -60,16 +60,16 @@ def sync_schedule(sport: str, league: str):
 def sync_players(sport: str, league: str, team_key: str):
 	logger.info(f'Syncing {team_key} roster...')
 	client = ESPNClient(sport, league)
-	players = client.get_roster(team_key)
+	_, upsert_players = client.get_roster(team_key)
 
-	logger.info(f'Upserting {len(players)} player(s)')
+	logger.info(f'Upserting {len(upsert_players)} player(s)')
 	try:
 		team = Team.objects.get(team_key=team_key)
 	except Team.DoesNotExist:
 		logger.warning(f'Missing team record for {team_key}')
 		return
 
-	for player in players:
+	for player in upsert_players:
 		Player.objects.update_or_create(
 			espn_id=player.espn_id,
 			defaults={
