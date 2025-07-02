d9a27cb2edfeedaaa0f2440017d98be96e34415e
Noah Roberts <noahkrob97@gmail.com>
2025-07-02 16:12:13 -0500

    changed celery namespace

--- backend/core/celery.py
+++ backend/core/celery.py
@@ -4,6 +4,6 @@ from celery import Celery
 env = os.getenv('DJANGO_ENV', 'dev')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'core.settings.{env}')
 
-app = Celery('backend')
-app.config_from_object( 'django.conf:settings', namespace='CELERY')
+app = Celery('arbadillo')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
