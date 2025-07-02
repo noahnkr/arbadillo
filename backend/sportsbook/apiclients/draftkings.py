db82192c494f61df7f8440434b7cf28ca83eccf9
Noah Roberts <noahkrob97@gmail.com>
2025-07-02 16:51:41 -0500

    fix: draftkings playwright_request fetch method drastically improved response time

--- backend/sportsbook/apiclients/draftkings.py
+++ backend/sportsbook/apiclients/draftkings.py
@@ -3,6 +3,7 @@ from dateutil.parser import isoparse
 from .base import SportsbookClient
 
 from common.utils.sportsbook_helpers import create_event_key, get_team_key
+from common.utils.client import get_browser
 from common.exceptions import NormalizationError
 
 class DraftKingsClient(SportsbookClient):
@@ -14,6 +15,7 @@ class DraftKingsClient(SportsbookClient):
 
 	def __init__(self, sport, league):
 		super().__init__(self.NAME, sport, league)
+		self.context = get_browser().new_context()
 	
 	def _get(self, path):
 		url = self.BASE_URL + path
@@ -21,7 +23,12 @@ class DraftKingsClient(SportsbookClient):
 			'origin': 'https://sportsbook.draftkings.com',
 			'referer': 'https://sportsbook.draftkings.com',
 		}
-		return super()._get(url, headers=headers)
+		return super()._get(
+			url, 
+			headers=headers,
+            method='playwright_request',
+            context=self.context
+		)
 	
 	def get_events(self):
 		league_url = f'/leagues/{self.LEAGUE_ID_MAP[self.league]}'
