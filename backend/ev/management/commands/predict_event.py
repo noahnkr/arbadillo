b1accccc81744bc1888bad1c44a152fbf5c336aa
Noah Roberts <noahkrob97@gmail.com>
2025-08-04 02:55:10 -0500

    feat: store predictions to output file

--- backend/ev/management/commands/predict_event.py
+++ backend/ev/management/commands/predict_event.py
@@ -1,5 +1,7 @@
 import pprint
+import os
 from django.core.management.base import BaseCommand
+from django.conf import settings
 from ev.ml.predict import predict_events
 from datetime import datetime
 
@@ -12,6 +14,7 @@ class Command(BaseCommand):
         parser.add_argument('--season', type=int, required=True)
         parser.add_argument('--after', type=str, help='Filter events after this date (YYYY-MM-DD)')
         parser.add_argument('--model-name', type=str, required=True)
+        parser.add_argument('--output-path', type=str)
 
     def handle(self, *args, **options):
         market = options['market']
@@ -21,6 +24,10 @@ class Command(BaseCommand):
         after_date = datetime.fromisoformat(after) if after else None
         model_name = options['model_name']
 
-        predictions = predict_events(market, league, season, cutoff=after_date, model_name=model_name)
+        output_path = options['output_path']
+
+        output = os.path.join(settings.BASE_DIR, output_path)
+
+        predictions = predict_events(market, league, season, cutoff=after_date, model_name=model_name, output_path=output)
         self.stdout.write(self.style.SUCCESS(f'{len(predictions)} predictions made.'))
         pprint.pprint(predictions)
