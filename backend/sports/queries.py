8ad72fb410c5a399a11ce1c89a088f999edf5d9b
Noah Roberts <noahkrob97@gmail.com>
2025-08-03 19:04:05 -0500

    feat: abstract `_get_stats` function with team and opponent stats

--- backend/sports/queries.py
+++ backend/sports/queries.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 from django.db.models import Model
-from sports.models import Event
+from sports.models import Event, TeamStat, PlayerStat
 from common.constants.aliases import REVERSE_STATS_LOOKUP, EFFICIENCY_ALIASES
 
 def _get_stats(model: Model, league: str, season: int, key_field: str, entity_key: str, event_key: str = None):
@@ -10,30 +10,30 @@ def _get_stats(model: Model, league: str, season: int, key_field: str, entity_ke
     if event_key:
         filters['event_key'] = event_key
     else:
-        filters['event_key__in'] = Event.objects.filter(
-            league=league,
-            season=season
-        ).values_list('event_key', flat=True)
+        filters['event_key__in'] = _get_events(league, season).values_list('event_key', flat=True)
 
     return model.objects.filter(**filters).values()
 
 
-def get_event_stats(model: Model, league: str, season: str, key_field: str, entity_key: str, event_key: str):
-    stats = _get_stats(model, league, season, key_field, entity_key, event_key).values('stat_name', 'value')
-    return { s['stat_name']: s['value'] for s in stats }
+def _get_events(league: str, season: int, team_key: str = None):
+    filters = { 'league': league, 'season': season }
+    if team_key:
+        events = Event.objects.filter(**filters, away_team=team_key) | Event.objects.filter(**filters, home_team=team_key)
+    else:
+        events = Event.objects.filter(**filters)
+
+    return events
 
 
-def get_season_stats(model: Model, league: str, season: str, key_field: str, entity_key: str):
-    stats = _get_stats(model, league, season, key_field, entity_key)
-
+def _aggregate_season_stats(league: str, season_stats: list):
     stat_sums = defaultdict(float)
     stat_counts = defaultdict(int)
 
-    for stat in stats:
+    for stat in season_stats:
         stat_sums[stat['stat_name']] += stat['value']
         stat_counts[stat['stat_name']] += 1
 
-    season_stats = {}
+    aggregate = {}
 
     # Compute rate for efficiency stats
     for conv, att in EFFICIENCY_ALIASES.values():
@@ -41,45 +41,83 @@ def get_season_stats(model: Model, league: str, season: str, key_field: str, ent
             conv_sum = stat_sums.get(conv, 0)
             att_sum = stat_sums.get(att, 1)
             eff_stat_name, _ = REVERSE_STATS_LOOKUP[(league, (conv, att))]
-            season_stats[eff_stat_name] = round(conv_sum / att_sum, 3)
-
-    # Compute season-level net or average for each respective stat type
-    excluded = {v for pair in EFFICIENCY_ALIASES.values() for v in pair}
-    for stat, stat_sum in stat_sums.items():
-        _, stat_type = REVERSE_STATS_LOOKUP[(league, stat)]
-        if stat not in excluded:
-            if stat_type == 'volume':
-                season_stats[f'net_{stat}'] = stat_sum
-            elif stat_type in {'rate', 'time'}:
-                season_stats[f'avg_{stat}'] = round(stat_sum / stat_counts[stat], 3)
-
-    return season_stats
-
-
-def get_season_stats_aggregate(model: Model, league: str, season: str, key_field: str, entity_key: str):
-    stats = _get_stats(model, league, season, key_field, entity_key)
-
-    stat_sums = defaultdict(float)
-    stat_counts = defaultdict(int)
-
-    for stat in stats:
-        stat_sums[stat['stat_name']] += stat['value']
-        stat_counts[stat['stat_name']] += 1
-
-    season_aggregate = {}
-
-    # Compute rate for efficiency stats
-    for conv, att in EFFICIENCY_ALIASES.values():
-        if conv in stat_sums and att in stat_sums:
-            conv_sum = stat_sums.get(conv, 0)
-            att_sum = stat_sums.get(att, 1)
-            eff_stat_name, _ = REVERSE_STATS_LOOKUP[(league, (conv, att))]
-            season_aggregate[eff_stat_name] = round(conv_sum / att_sum, 3)
+            aggregate[eff_stat_name] = round(conv_sum / att_sum, 3)
 
     # Compute per-game average for other stats
     excluded = {v for pair in EFFICIENCY_ALIASES.values() for v in pair}
     for stat, stat_sum in stat_sums.items():
         if stat not in excluded:
-            season_aggregate[f'avg_{stat}'] = round(stat_sum / stat_counts[stat], 3)
+            aggregate[stat] = round(stat_sum / stat_counts[stat], 3)
     
-    return season_aggregate
+    return aggregate
+
+
+def get_team_event_stats(league: str, season: str, team_key: str, event_key: str):
+    stats = _get_stats(
+        model=TeamStat, 
+        league=league,
+        season=season, 
+        key_field='team_key',
+        entity_key=team_key,
+        event_key=event_key
+    )
+    return { s['stat_name']: s['value'] for s in stats }
+
+
+def get_player_event_stats(league: str, season: str, player_key: str, event_key: str):
+    stats = _get_stats(
+        model=PlayerStat, 
+        league=league,
+        season=season, 
+        key_field='player_key',
+        entity_key=player_key,
+        event_key=event_key
+    )
+    return { s['stat_name']: s['value'] for s in stats }
+
+
+def get_team_season_stats(league: str, season: str, team_key: str):
+    stats = _get_stats(
+        model=TeamStat, 
+        league=league,
+        season=season, 
+        key_field='team_key',
+        entity_key=team_key,
+    )
+    return _aggregate_season_stats(league, stats)
+
+ 
+def get_player_season_stats(league: str, season: str, player_key: str):
+    stats = _get_stats(
+        model=TeamStat, 
+        league=league,
+        season=season, 
+        key_field='player_key',
+        entity_key=player_key,
+    )
+    return _aggregate_season_stats(league, stats)
+
+
+def get_team_opponent_season_stats(league: str, season: str, team_key: str):
+    events = _get_events(league, season, team_key)
+    event_opponents = {}
+
+    for event in events:
+        if event.away_team == team_key:
+            event_opponents[event.event_key] = event.home_team
+        else:
+            event_opponents[event.event_key] = event.away_team
+
+    opponent_stats = []
+    for event_key, opponent in event_opponents.items():
+        stats = _get_stats(
+            model=TeamStat,
+            league=league,
+            season=season,
+            key_field='team_key',
+            entity_key=opponent,
+            event_key=event_key
+        )
+        opponent_stats.extend(stats)
+    
+    return _aggregate_season_stats(league, opponent_stats)
